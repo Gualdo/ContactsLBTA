ddb8d74946c21dd71244b5a173983c719cc3599f
Eduardo De La Cruz <eduardo.delacruz@gft.com>
2019-02-27 11:08:53 +0100

    Initial Commit

---
+++ ContactsLBTA/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContactsLBTA
+//
+//  Created by De La Cruz, Eduardo on 27/02/2019.
+//  Copyright © 2019 De La Cruz, Eduardo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ContactsLBTATests/ContactsLBTATests.swift
@@ -0,0 +1,34 @@
+//
+//  ContactsLBTATests.swift
+//  ContactsLBTATests
+//
+//  Created by De La Cruz, Eduardo on 27/02/2019.
+//  Copyright © 2019 De La Cruz, Eduardo. All rights reserved.
+//
+
+import XCTest
+@testable import ContactsLBTA
+
+class ContactsLBTATests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

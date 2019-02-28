14164ace9a6b59c16a2a8085e2f7a05f7b1e822a
Eduardo De La Cruz <gualdodelacruz@gmail.com>
2019-02-28 10:29:30 +0100

    WIP Animations

--- ContactsLBTA/ViewController.swift
+++ ContactsLBTA/ViewController.swift
@@ -8,13 +8,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    let cellID = "cellID"
+    
+    let twoDimentionalArray = [["Amy", "Bill", "Zack", "Steve", "Jack", "Jill", "Mary"], ["Carl", "Chris", "Christina", "Cameron"], ["David", "Dan"], ["Patric", "Patty"]]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Show IndexPath", style: .plain, target: self, action: #selector(handleShowIndexPath))
+        navigationItem.title = "Contacts"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
     }
-
-
+    
+    @objc func handleShowIndexPath() {
+        var pairIndicesArray = [IndexPath]()
+        var oddIndicesArray = [IndexPath]()
+        
+        for section in twoDimentionalArray.indices {
+            for row in twoDimentionalArray[section].indices {
+                if row % 2 == 0 {
+                    pairIndicesArray.append(IndexPath(row: row, section: section))
+                } else {
+                    oddIndicesArray.append(IndexPath(row: row, section: section))
+                }
+            }
+        }
+        
+        tableView.reloadRows(at: pairIndicesArray, with: .left)
+        tableView.reloadRows(at: oddIndicesArray, with: .right)
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let label = UILabel()
+        label.text = "Header"
+        label.backgroundColor = .lightGray
+        return label
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return twoDimentionalArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return twoDimentionalArray[section].count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
+        let name = twoDimentionalArray[indexPath.section][indexPath.row]
+        
+        cell.textLabel?.text = "\(name) Section: \(indexPath.section) Row: \(indexPath.row)"
+        
+        return cell
+    }
 }
-
